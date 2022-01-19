1e14922440fc7966b160c75f36ba9630fa9e5f84
Zikri Muhammad Madani <73283220+gdearya2@users.noreply.github.com>
2022-01-19 22:16:44 +0700

    Update functions.php
    1. menambahkan fungsi query untuk menampilkan data
    2. menambahkan fungsi tambah untuk menambahkan data ke database

--- functions.php
+++ functions.php
@@ -7,3 +7,28 @@ function login($query)
     $validasi = mysqli_query($conn, $query);
     return $validasi;
 }
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data)
+{
+    global $conn;
+    $name = htmlspecialchars($data["name"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $angkatan = htmlspecialchars($data["angkatan"]);
+    $departemen = htmlspecialchars($data["departemen"]);
+    $email =  htmlspecialchars($data["email"]);
+
+    $query = "INSERT INTO anggota VALUES ('','$name','$nim','$angkatan','$departemen','$email')";
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
