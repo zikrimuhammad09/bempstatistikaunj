f1c84fb76e89a3aab64bcf693a535f7423505fd1
Zikri Muhammad Madani <73283220+gdearya2@users.noreply.github.com>
2022-01-19 22:44:28 +0700

    menambahkan logo icon website

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $semuaAnggota = query("SELECT * FROM anggota");
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>BEMP STATISTIKA UNJ</title>
+  <!-- Icon Website -->
+  <link rel="icon" href="img/LOGO BEMP STAT UNJ.png">
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
