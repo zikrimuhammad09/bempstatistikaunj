2673da065558910a3862ff68e0d94791160f2f45
Zikri Muhammad Madani <73283220+gdearya2@users.noreply.github.com>
2022-01-20 23:12:24 +0700

    Create script.js
    file js untuk fitur live search dan delete alert dengan ajax di anggota.php

---
+++ script.js
@@ -0,0 +1,46 @@
+// Dengan Jquery
+$(document).ready(() => {
+  // event ketika keyword ditulis
+  $("#keyword").on("keyup", () => {
+    $("#container").load("ajax/search.php?keyword=" + $("#keyword").val());
+  });
+});
+
+//event ketika menghapus data
+$(".hapus").on("click", function (e) {
+  e.preventDefault();
+  Swal.fire({
+    title: "Yakin ingin menghapus data?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Hapus",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $("#container").load("ajax/" + $(this).attr("href"));
+    }
+  });
+});
+
+// Dengan Javascript biasa
+// // ambil elemen2 yang dibutuhkan
+// var keyword = document.getElementById("keyword");
+// var container = document.getElementById("container");
+
+// // tambahkan event ketikaa keyword ditulis
+// keyword.addEventListener("keyup", () => {
+//   // buat object ajax
+//   var xhr = new XMLHttpRequest();
+
+//   // cek kesiapan ajax
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       container.innerHTML = xhr.responseText;
+//     }
+//   };
+
+//   // eksekusi ajax
+//   xhr.open("GET", `ajax/anggota.php?keyword=${keyword.value}`, true);
+//   xhr.send();
+// });
