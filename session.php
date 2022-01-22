ea29d3430e286c50e492e985c60f35f9ad13b0ff
Zikri Muhammad Madani <73283220+gdearya2@users.noreply.github.com>
2022-01-22 10:49:40 +0700

    Update session.php
    mengubah logika pada session

--- session.php
+++ session.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if ($_SESSION["login"] == false) {
-    header("Location: login.php");
-    exit();
+if (!isset($_SESSION["login"])) {
+    header("Location: login");
+    exit;
 }
